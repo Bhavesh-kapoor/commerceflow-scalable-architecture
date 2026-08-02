646c6f395271deef10b070998320ecf8f1925be8
Bhavesh Kapoor <bhaveshkapoor@Bhaveshs-MacBook-Air.local>
2026-08-02 08:46:28 +0530

    refactor(docker):mailhog and notification worker

--- backend/app/Jobs/SendWelcomeEmail.php
+++ backend/app/Jobs/SendWelcomeEmail.php
@@ -9,6 +9,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Log;
 
+
 class SendWelcomeEmail implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
